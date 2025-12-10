6055264a63e8dae2e635580f47596b5e5f8c08a0
Zaidfarooqui72 <reviewandearn092@gmail.com>
2025-12-10 09:15:43 +0530

    Added assignment questions of part b

--- Assignment_day2/part_a.js
+++ Assignment_day2/part_a.js
@@ -10,10 +10,10 @@ console.log(`Hello, ${name}! Welcome to JavaScript programming.`);
 
 // 3: Check even or odd
 let arr = [1, 2, 3, 4, 5];
-for(i of arr){
-    if(i % 2 === 0){
+for (i of arr) {
+    if (i % 2 === 0) {
         console.log(i, "is even");
-    }else{
+    } else {
         console.log(i, "is odd");
     }
 }
@@ -22,20 +22,20 @@ console.log("-----------------------------");
 
 // 4: Celcius to Fahrenheit conversion
 let cel = 98;
-let far = (cel * 9/5) + 32;
+let far = (cel * 9 / 5) + 32;
 console.log("Temperature in Fahrenheit is: ", far);
 console.log("-----------------------------");
 
 // 5: Find the largest number among three numbers
 let num1 = 15, num2 = 20, num3 = 5;
 let largest;
-if(num1 >= num2 && num1 >= num3){
+if (num1 >= num2 && num1 >= num3) {
     largest = num1;
 }
-else if(num2 >= num1 && num2 >= num3){
+else if (num2 >= num1 && num2 >= num3) {
     largest = num2;
-}   
-else{
+}
+else {
     largest = num3;
 }
 console.log("The Largest number among", num1, ",", num2, "and", num3, "is:", largest);
@@ -44,7 +44,7 @@ console.log("-----------------------------");
 // 6: Count the length of a string
 let str = "Hello World";
 let count = 0;
-for(i of str){
+for (i of str) {
     count++;
 }
 console.log("The length of the string ", str, "is:", count);
@@ -52,7 +52,7 @@ console.log("-----------------------------");
 
 // 7: Toggle a boolean value
 let boolean = true;
-if(boolean){
+if (boolean) {
     boolean = false;
     console.log("The value is now:", boolean);
 }
@@ -67,11 +67,11 @@ console.log("-----------------------------");
 
 // 9: Positive, Negative or Zero
 let numb = 7;
-if(numb > 0){
+if (numb > 0) {
     console.log(numb, "is a positive number.");
-}else if(numb < 0){
+} else if (numb < 0) {
     console.log(numb, "is a negative number.");
-}else{
+} else {
     console.log("The number is zero.");
 }
 console.log("-----------------------------");
@@ -84,7 +84,7 @@ console.log("-----------------------------");
 
 // 11: Print multiplication table of a number
 let tableOf5 = "";
-for(let i = 1; i <= 10; i++){
+for (let i = 1; i <= 10; i++) {
     tableOf5 += `5 x ${i} = ${5 * i}\n`;
 }
 console.log("Table of 5:\n", tableOf5);
@@ -93,14 +93,16 @@ console.log("-----------------------------");
 
 // 12: Sum of First 10 Natural Numbers
 let sum = 0;
-for(let i = 1; i <= 10; i++){
+for (let i = 1; i <= 10; i++) {
     sum += i;
 }
 console.log("Sum of First 10 Natural Numbers is:", sum);
 
 // 13: Switch case for days of the week
-let  day = 3;
-switch(day){
+//let u_day = prompt("Enter the Day of Weeks in Number: ");
+// let day = parseInt(u_day);
+let day = 3;
+switch (day) {
     case 1:
         console.log("1: " + "Sunday");
         break;
@@ -124,16 +126,17 @@ switch(day){
         break;
     default:
         console.log("Invalid day number!");
-} 
+}
 console.log("-----------------------------");
 
 // 14: Factorial of a number
-
-function fact(n){
-    if(n===0 || n===1){
+//let input_factn = prompt("Enter The Number: ");
+//let factn = parseInt(user_factn);
+function fact(n) {
+    if (n === 0 || n === 1) {
         return 1;
     }
-    return n*fact(n-1);
+    return n * fact(n - 1);
 }
 let factn = 5;
 console.log("Factorial of", factn, "is:", fact(factn));
@@ -141,8 +144,8 @@ console.log("-----------------------------");
 
 // 15: Check voting eligibility
 let age = 17;
-if(age >= 18){
+if (age >= 18) {
     console.log("You are eligible to vote.");
-}else{
+} else {
     console.log("You are not eligible to vote.");
 }
--- Assignment_day2/part_b.js
+++ Assignment_day2/part_b.js
@@ -3,20 +3,20 @@
 // 16: Check if a number is prime
 let prime = 33;
 let isPrime = true;
-if(prime < 2){
+if (prime < 2) {
     console.log(`${prime} is not a prime!`);
-}else{
-    for(let i = prime; i >= prime/2; i--){
-        if(prime % i === 0){
+} else {
+    for (let i = prime; i >= prime / 2; i--) {
+        if (prime % i === 0) {
             isPrime = false;
-        }else{
+        } else {
             isPrime = true;
         }
     }
-} 
-if(isPrime){
+}
+if (isPrime) {
     console.log(`${prime} is a prime number.`);
-}else{
+} else {
     console.log(`${prime} is not a prime number.`);
 }
 console.log("-----------------------------");
@@ -25,9 +25,69 @@ console.log("-----------------------------");
 let num = 232;
 let arr = num.toString().split('').map(Number) || []; // Converting number to array of digits
 let sumofdigits = 0;
-for(i of arr){
+for (i of arr) {
     sumofdigits += i;
 }
 console.log("Sum of digits of", num, "is:", sumofdigits);
 console.log("-----------------------------");
 
+// 18: To Reverse a string 
+let str = "United Group of Institution";
+console.log("Original string = ", str);
+let rev = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+    rev += str[i];
+}
+console.log("Reversed String = ", rev);
+console.log("------------------------------");
+
+// 19: Grades
+// let user_marks = prompt("Enter Your Marks: ");
+// let marks = parseInt(user_marks);
+// We cannot use prompt in Node.js environment, so directly assigning a value
+let marks = 85;
+if (isNaN(marks) || marks < 0 || marks > 100) {
+    console.log("Please enter valid marks between 0 and 100.");
+} else {
+    if (marks >= 90) {
+        console.log("Grade A");
+    } else if (marks >= 75 && marks < 90) {
+        console.log("Grade B");
+    } else if (marks >= 50 && marks < 75) {
+        console.log("Grade C");
+    } else {
+        console.log("Fail");
+    }
+}
+
+console.log("-----------------------------");
+
+// 20: mathematical function
+
+function calculate_fun(x, y, o){
+    if(o === "+"){
+        console.log("Addition",x + "+" + y + "=", x+y);
+    }else if(o === "-"){
+        console.log("Subtraction",x + "-" + y + "=", x-y);
+    }else{
+        console.log("Division",x + "/" + y + "=", x/y);
+    }
+}
+calculate_fun(10,5,"+");
+calculate_fun(10,5,"-");
+calculate_fun(10,5,"/");
+console.log("-----------------------------");
+
+// 21: Count Wovels
+let original_letter = "United";
+let letter = original_letter.toLowerCase();
+let count_vowel = 0;
+for(i of letter){
+    if(i === "a" || i === "e" || i === "i" || i === "o" || i === "u"){
+        count_vowel++;
+    }
+}
+console.log("Wovels in "+ letter + "=  " + count_vowel);
+
+// Done 21 out of 25
