dccda1adea3e6e92843f6a62b33ec945ebcdef72
Zaidfarooqui72 <reviewandearn092@gmail.com>
2025-12-11 11:00:07 +0530

    new script file

---
+++ In_Class/script.js
@@ -0,0 +1,10 @@
+// Date 
+let myDate = new Date();
+console.log("Date:",myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log("JSON:",myDate.toJSON());
